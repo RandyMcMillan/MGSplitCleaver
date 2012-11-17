7f21d49b15cb89e3776fcc978e79e722b6a00017
Randy McMillan <randymcmillan@DeepSpaceMacBookPro.local>
2012-11-17 18:21:58 -0500

    Prep for true Universal Support

--- main.m
+++ main.m
@@ -7,11 +7,25 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "MGSplitViewAppDelegate.h"
 
+/* original
 int main(int argc, char *argv[]) {
     
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
     int retVal = UIApplicationMain(argc, argv, nil, nil);
     [pool release];
     return retVal;
+}
+*/
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        if (IS_IPAD()) {
+                return UIApplicationMain(argc, argv, nil, NSStringFromClass([MGSplitViewAppDelegate class]));
+        } else {
+                //return UIApplicationMain(argc, argv, nil, NSStringFromClass([UniversalExampleAppDelegate_Phone class]));
+        }
+    }
 }
