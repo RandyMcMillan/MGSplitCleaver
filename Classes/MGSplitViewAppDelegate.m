f0c5eb10709318879834b7f9ecae9136ec518a50
Randy McMillan <randymcmillan@DeepSpaceMacBookPro.local>
2012-11-17 16:32:18 -0500

    fix orientation issue in iOS6
    	// Add the split view controller's view to the window and display.
    
    #if __IPHONE_OS_VERSION_MAX_ALLOWED > 6000
    		[window setRootViewController:splitViewController];
    #else
    		[window addSubview:splitViewController.view];
    #endif
    
    	//    [window addSubview:splitViewController.view];
    	[window makeKeyAndVisible];

--- Classes/MGSplitViewAppDelegate.m
+++ Classes/MGSplitViewAppDelegate.m
@@ -13,36 +13,38 @@
 
 @implementation MGSplitViewAppDelegate
 
-
 @synthesize window, splitViewController, rootViewController, detailViewController;
-
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    // Add the split view controller's view to the window and display.
-    [window addSubview:splitViewController.view];
-    [window makeKeyAndVisible];
-	
+	// Add the split view controller's view to the window and display.
+
+#if __IPHONE_OS_VERSION_MAX_ALLOWED > 6000
+		[window setRootViewController:splitViewController];
+#else
+		[window addSubview:splitViewController.view];
+#endif
+
+	//    [window addSubview:splitViewController.view];
+	[window makeKeyAndVisible];
+
 	[rootViewController performSelector:@selector(selectFirstRow) withObject:nil afterDelay:0];
 	[detailViewController performSelector:@selector(configureView) withObject:nil afterDelay:0];
-	
-	if (NO) { // whether to allow dragging the divider to move the split.
-		splitViewController.splitWidth = 15.0; // make it wide enough to actually drag!
-		splitViewController.allowsDraggingDivider = YES;
-	}
-	
-    return YES;
-}
 
+	if (NO) {												// whether to allow dragging the divider to move the split.
+		splitViewController.splitWidth				= 15.0;	// make it wide enough to actually drag!
+		splitViewController.allowsDraggingDivider	= YES;
+	}
+
+	return YES;
+}
 
 - (void)dealloc
 {
-    [splitViewController release];
-    [window release];
-	
-    [super dealloc];
+	[splitViewController release];
+	[window release];
+
+	[super dealloc];
 }
 
-
 @end
-
